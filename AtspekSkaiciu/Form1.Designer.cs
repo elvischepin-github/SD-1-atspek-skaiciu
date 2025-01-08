5c71a345fa76095b38a6163019b3ab5844557624
Elvis Cepinskas <111094197+elvischepin-github@users.noreply.github.com>
2025-01-08 11:35:19 +0200

    Add files via upload

---
+++ AtspekSkaiciu/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace AtspekSkaiciu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(7, 6);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Pradėti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Spėti";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(231, 185);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Naujas";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(312, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 40);
+            this.label1.TabIndex = 4;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(312, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 40);
+            this.label2.TabIndex = 5;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(312, 134);
+            this.label3.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 40);
+            this.label3.TabIndex = 6;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(88, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 28);
+            this.label4.TabIndex = 7;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(7, 32);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(299, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(7, 83);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(299, 45);
+            this.trackBar2.TabIndex = 9;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(7, 134);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(299, 45);
+            this.trackBar3.TabIndex = 10;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 220);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Atspėk skaičių!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+    }
+}
+
---
+++ AtspekSkaiciu/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtspekSkaiciu
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        int atsitiktinisSkaicius;
+        public Form1()
+        {
+            InitializeComponent();
+
+            trackBar1.Enabled = false;
+            trackBar2.Enabled = false;
+            trackBar3.Enabled = false;
+            button2.Enabled = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pasirinktasSkaicius = Convert.ToInt32(numericUpDown1.Value);
+            atsitiktinisSkaicius = random.Next(1, pasirinktasSkaicius);
+
+            numericUpDown1.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+            trackBar1.Minimum = 1;
+            trackBar1.Maximum = Convert.ToInt32(numericUpDown1.Value);
+            trackBar1.Value = 1;
+            label1.Text = trackBar1.Value.ToString();
+
+            trackBar2.Minimum = 1;
+            trackBar2.Maximum = Convert.ToInt32(numericUpDown1.Value);
+            trackBar2.Value = Convert.ToInt32(numericUpDown1.Value);
+            label2.Text = trackBar2.Value.ToString();
+
+            trackBar3.Enabled = true;
+            trackBar3.Minimum = 1;
+            trackBar3.Maximum = Convert.ToInt32(numericUpDown1.Value);
+            trackBar3.Value = 1;
+            label3 .Text = trackBar3.Value.ToString();
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            int minValue = trackBar1.Value;
+            int maxValue = trackBar2.Value;
+
+            if (trackBar3.Value < minValue)
+            {
+                trackBar3.Value = minValue;
+            }
+            else if (trackBar3.Value > maxValue)
+            {
+                trackBar3.Value = maxValue;
+            }
+
+            label3.Text = trackBar3.Value.ToString();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            label2.Text = trackBar2.Value.ToString();
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int mazesnysis = 1, didesnysis = Convert.ToInt32(numericUpDown1.Value);
+
+            if (trackBar3.Value == atsitiktinisSkaicius)
+            {
+                label4.BackColor = Color.Green;
+                label4.Text = "Atspėjote!";
+                button2.Enabled = false;
+                button3.Enabled = true;
+            }
+            else if (trackBar3.Value < atsitiktinisSkaicius)
+            {
+                label4.BackColor = Color.Red;
+                label4.Text = "Skaičius yra didesnis!";
+
+                if(didesnysis > trackBar3.Value)
+                {
+                    didesnysis = trackBar3.Value;
+                    trackBar1.Value = didesnysis;
+                }
+            }
+            else if (trackBar3.Value > atsitiktinisSkaicius)
+            {
+                label4.BackColor = Color.Red;
+                label4.Text = "Skaičius yra mažesnis!";
+
+                if (mazesnysis < trackBar3.Value)
+                {
+                    mazesnysis = trackBar3.Value;
+                    trackBar2.Value = mazesnysis;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Enabled = true;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            label4.Text = "";
+            label4.BackColor = SystemColors.Control;
+            trackBar1.Enabled = false;
+            trackBar1.Value = 1;
+            label1.Text = "";
+            trackBar2.Enabled = false;
+            trackBar2.Value = 1;
+            label2.Text = "";
+            trackBar3.Enabled = false;
+            trackBar3.Value = 1;
+            label3.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //numericUpDown1.Minimum = 10;
+            //numericUpDown1.Maximum = 10000;
+        }
+    }
+}
